312f7ac887286de346bfa5c0eb1d0c9cc9339026
wueiz <wuezmonster@gmail.com>
2017-08-05 20:16:13 +0800

    update the fengsheng to template path

--- siteservice/index/views.py
+++ siteservice/index/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render, get_object_or_404, HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404, HttpResponse, Http404, HttpResponseRedirect
 from .models import Articles
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 
 def index_render(requests):
+    host = requests.get_host()
+    if host == "http://www.dsgjjy.com/":
+        return HttpResponseRedirect("/ds")
     articles_notice = Articles.objects.filter(article_type='1', isPublish=True).order_by("-time")[:5]
     articles_new = Articles.objects.filter(article_type='2', isPublish=True).order_by("-time")[:5]
-    return render(requests, 'index.html', locals())
+    return render(requests, 'fengsheng/index.html', locals())
 
 def article_render(requests):
     a_id = requests.GET.get('article_id')
     article = get_object_or_404(Articles, pk=a_id)
-    return render(requests, 'article.html', locals())
+    return render(requests, 'fengsheng/article.html', locals())
 
 def list_render(requests):
     list_type = requests.GET.get('list_type', '1')  # 1 or 2 , 1 is notices and the 2 is .........news
@@ -26,4 +29,4 @@ def list_render(requests):
         articles = paginator.page(1)
     except EmptyPage:
         articles = paginator.page(paginator.num_pages)
-    return render(requests, 'articleList.html', locals())
+    return render(requests, 'fengsheng/articleList.html', locals())
